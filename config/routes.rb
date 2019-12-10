6b6457e9bb287c6d85a56c7596dcf38ec56f8269
Malcolm <echelon1089@gmail.com>
2019-12-10 14:40:11 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get  '/signin' ,to: 'sessions#new'
   post '/session',to: 'sessions#create' 
   root  'sessions#index'
+  get '/attractions', to: 'rides#index', controller: :rides
+  #resources :rides, only: [:index]
   
   resources :users do
     post  '/delete', to: 'sessions#destroy'
